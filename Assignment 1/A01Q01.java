cac76d153fd54ea17724b0e65d80a8fa1379af41
Samridhi Sahu <sanskritis320@gmail.com>
2023-01-28 13:58:42 +0530

    Program added

---
+++ Assignment 1/A01Q01.java
@@ -0,0 +1,7 @@
+// 1. Write a JAVA program to print Hello Students on the screen.
+
+public class A01Q01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Students");
+    }
+}
