598f59cd8213ad16fc9a481f04cc5c774fa24d2c
Samridhi Sahu <98205847+samridhi-sahu@users.noreply.github.com>
2023-03-26 00:48:48 +0530

    added

---
+++ Assignment 57/Q8.java
@@ -0,0 +1,86 @@
+// 8. Write a Program to reverse the Doubly Linked List.
+
+public class Q8 {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node() {
+            next = null;
+            prev = null;
+        }
+    }
+
+    static class linkedlist {
+        Node head;
+
+        public void insert(int element) {
+            Node n = new Node();
+            n.data = element;
+            n.prev = null;
+            n.next = null;
+            if (head == null) {
+                head = n;
+            } else {
+                Node p = head;
+                while (p.next != null) {
+                    p = p.next;
+                }
+                p.next = n;
+                n.prev = p;
+            }
+        }
+
+        public void reverse() {
+            Node p = head;
+            while (p.next != null) {
+                p = p.next;
+            }
+            Node q = head;
+            while (p != q) {
+                Node n = new Node();
+                n.data = p.data;
+                n.next = q;
+                if (q == head) {
+                    n.prev = null;
+                    head.prev = n;
+                    head = n;
+                } else {
+                    n.prev = q.prev;
+                    q.prev.next = n;
+                    q.prev = n;
+                }
+                p.prev.next = null;
+                p = p.prev;
+            }
+        }
+
+        public void display() {
+            Node p = head;
+            while (p != null) {
+                System.out.print(p.data + " ");
+                p = p.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+        list.insert(7);
+        list.insert(8);
+        list.insert(9);
+        list.insert(10);
+        System.out.println("Before Reversing the given list, elements are : ");
+        list.display();
+        list.reverse();
+        System.out.println("\nAfter Reversing the given list, elements are : ");
+        list.display();
+    }
+}
---
+++ Assignment 57/Q9.java
@@ -0,0 +1,74 @@
+// 9. Given a Doubly linked list containing N nodes, the task is to remove all the nodes from the list which contains elements whose digit sum is even.
+
+public class Q9 {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node() {
+            next = null;
+            prev = null;
+        }
+    }
+
+    static class linkedlist {
+        Node head;
+
+        public void insert(int element) {
+            Node n = new Node();
+            n.data = element;
+            n.prev = null;
+            n.next = null;
+            if (head == null) {
+                head = n;
+            } else {
+                Node p = head;
+                while (p.next != null) {
+                    p = p.next;
+                }
+                p.next = n;
+                n.prev = p;
+            }
+        }
+
+        public void removing() {
+            Node p = head;
+            while (p.next != null) {
+                int temp = p.data;
+                int sum = 0;
+                while (temp > 0) {
+                    sum += temp % 10;
+                    temp = temp / 10;
+                }
+                if (sum % 2 == 0) {
+                    p.prev.next = p.next;
+                    p.next.prev = p.prev;
+                }
+                p = p.next;
+            }
+        }
+
+        public void display() {
+            Node p = head;
+            while (p != null) {
+                System.out.print(p.data + " ");
+                p = p.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+        list.insert(18);
+        list.insert(15);
+        list.insert(8);
+        list.insert(14);
+        list.insert(9);
+        System.out.println("Before Removing, elements are : ");
+        list.display();
+        list.removing();
+        System.out.println("\nAfter Removing, elements are : ");
+        list.display();
+    }
+}
